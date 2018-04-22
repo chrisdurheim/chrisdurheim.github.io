ed1b5d573142fc0de0044e25a3daae3a6a631f0e
Chris Durheim <christopherdurheim@gmail.com>
2018-04-21 22:35:31 -0500

    remove console log

--- assets/scripts/page.js
+++ assets/scripts/page.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   function smoothScroll(id) {
-    console.log(id);
     document.querySelector(id).scrollIntoView({
       block: "start",
       inline: "nearest",
